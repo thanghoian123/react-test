e036c4607afcd66981f9e7442f1e90976df6af9e
thang pham <thangpham1117@gmail.com>
2022-10-14 13:19:13 +0700

    init husky

--- lint-staged.js
+++ lint-staged.js
@@ -1,6 +1,5 @@
 module.exports = {
     '*.{js,jsx,ts,tsx}': [
-        'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
         () => 'tsc-files --noEmit',
     ],
     // '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
